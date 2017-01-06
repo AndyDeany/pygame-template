b837baed7ad29e8f075251ebd3a7c95b00277c2b
AndyDeany <oneandydean@hotmail.com>
2017-01-06 14:17:36 +0000

    Add blinking cursor to TextInput and largely refactor it

---
+++ lib/helper.py
@@ -0,0 +1,14 @@
+class Helper(object):
+    """Class holding helper functions."""
+    def __init__(self, game):
+        self.game = game
+
+    def load_class_assets(self, calling_object, assets_dict):
+        """Loads class assets. Should only be calling if self.class_assets_loaded is False."""
+        try:
+            for attribute_name in assets_dict:
+                calling_class = calling_object.__class__
+                setattr(calling_class, attribute_name, assets_dict[attribute_name])
+        except Exception as self.game.error:
+            self.game.log("Failed to load ", calling_class.__name__, " class assets")
+        setattr(calling_class, "class_assets_loaded", True)
