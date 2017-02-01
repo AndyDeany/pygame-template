8bac2b005818094bdfac46810b92826ca05ba4dd
AndyDeany <oneandydean@hotmail.com>
2017-02-01 14:20:37 +0000

    Add functionality to Game.log() to differentiate between fatal and non-fatal exceptions

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+import datetime
+import traceback
+
 from lib.game import Game
+from lib.caught_fatal_exception import CaughtFatalException
 
 if __name__ == "__main__":
     #! Decide how the window should be when the game opens. (fullscreen/borderless/windowed)?
     # How big should it be? User's monitor dimensions? Something else?
     try:
         game = Game((1280, 720))
-        game.run()
-    except CaughtFatalException:
-        pass
     except Exception:
-        # Catches all exceptions that weren't caught in the rest of the code
-        game.log("UNCAUGHT FATAL EXCEPTION")
+        with open("log.txt", "a") as error_log:
+            error_log.write("%s - UNCAUGHT FATAL EXCEPTION\n" % datetime.datetime.utcnow())
+            error_log.write(traceback.format_exc() + "\n")
+    else:
+        try:
+            game.run()
+        except CaughtFatalException:
+            pass
+        except Exception: # Catches all exceptions that weren't caught in the rest of the code
+            game.log("UNCAUGHT FATAL EXCEPTION")
