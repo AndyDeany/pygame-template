5fe43d126555486d8f34cae79abd764d93b9f669
AndyDeany <oneandydean@hotmail.com>
2017-09-09 23:42:24 +0100

    Add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -10,5 +10,13 @@ setup(
     url="https://github.com/AndyDeany/pygame-template",
     author=pygametemplate.__author__,
     author_email="oneandydean@hotmail.com",
-    packages=["pygametemplate"]
+    packages=["pygametemplate"],
+    classifiers=(
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: pygame"
+    )
 )
