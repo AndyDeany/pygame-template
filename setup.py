609bc6fbd1284c1b769c2e0548f6c65a97d144cd
AndyDeany <oneandydean@hotmail.com>
2017-09-09 18:42:47 +0100

    Add initial attempt at a setup.py file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+import pygametemplate
+
+
+setup(
+    name="pygametemplate",
+    version=pygametemplate.__version__,
+    description=pygametemplate.__doc__,
+    url="https://github.com/AndyDeany/pygame-template",
+    author=pygametemplate.__author__,
+    author_email="oneandydean@hotmail.com",
+    packages=["pygametemplate"]
+)
