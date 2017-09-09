b8e62632494a5e963d1f2e234f26c1349b48bba5
AndyDeany <oneandydean@hotmail.com>
2017-09-09 10:49:04 +0100

    Add helper.wrap_text() function

--- lib/helper.py
+++ lib/helper.py
@@ -12,3 +12,44 @@ class Helper(object):
         except Exception:
             self.game.log("Failed to load ", calling_class.__name__, " class assets")
         setattr(calling_class, "class_assets_loaded", True)
+
+    def wrap_text(self, text, font, max_width):
+        """
+        Returns an array of lines which can be blitted beneath each other
+        in the given font in a box of the given maximum width.
+        """
+        def wrap_paragraph(paragraph):
+            """Wraps text that doesn't contain newlines."""
+            def too_long(string):
+                return font.size(string)[0] > max_width
+
+            def raise_word_too_long_error(word):
+                raise ValueError("\"%s\" is too long to be wrapped." % word)
+
+            lines = []
+            words = paragraph.split()
+
+            line = words.pop(0)
+            if too_long(line):
+                raise_word_too_long_error(line)
+
+            for word in words:
+                if too_long(word):
+                    raise_word_too_long_error(word)
+
+                if too_long(" ".join((line, word))):
+                    lines.append(line)
+                    line = word
+                else:
+                    line = " ".join((line, word))
+
+            lines.append(line)
+            return lines
+
+        try:
+            paragraphs = text.split("\n")
+            return sum(map(wrap_paragraph, paragraphs), [])
+        except Exception as error:
+            fatal = not isinstance(error, ValueError)
+            self.game.log("Failed to wrap text: \"", text, "\"", fatal=fatal)
+            return ["error"]
