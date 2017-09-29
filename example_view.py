8a93d2d33955ae9340ccc503754976c1ea3c7232
AndyDeany <oneandydean@hotmail.com>
2017-09-29 18:18:43 +0100

    Add ExampleView for using in main.py

---
+++ example_view.py
@@ -0,0 +1,16 @@
+from pygametemplate.view import View
+
+
+class ExampleView(View):
+
+    def load(self):
+        pass
+
+    def unload(self):
+        pass
+
+    def logic(self):
+        pass
+
+    def draw(self):
+        pass
