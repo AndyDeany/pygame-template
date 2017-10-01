9291216b1b2dde182f29cecfc7b6288ade60608b
AndyDeany <oneandydean@hotmail.com>
2017-10-01 12:09:31 +0100

    Add TODO comment for removing TestGame class (doesn't do anything now)

--- spec/helper.py
+++ spec/helper.py
@@ -2,7 +2,7 @@ from pygametemplate import Game
 from example_view import ExampleView
 
 
-class TestGame(Game):
+class TestGame(Game):   # TODO: remove this useless class
     """An altered Game class for testing purposes."""
     def __init__(self, StartingView, resolution):
         super(TestGame, self).__init__(StartingView, resolution)
