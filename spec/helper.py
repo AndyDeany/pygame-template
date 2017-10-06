6d16f35ed6b21401bbfb5d233b8299585175901b
AndyDeany <oneandydean@hotmail.com>
2017-10-06 18:23:43 +0100

    Remove useless TestGame class in spec.helper module

--- spec/helper.py
+++ spec/helper.py
@@ -2,10 +2,4 @@ from pygametemplate import Game
 from example_view import ExampleView
 
 
-class TestGame(Game):   # TODO: remove this useless class
-    """An altered Game class for testing purposes."""
-    def __init__(self, StartingView, resolution):
-        super(TestGame, self).__init__(StartingView, resolution)
-
-
-game = TestGame(ExampleView, (1280, 720))
+game = Game(ExampleView, (1280, 720))
