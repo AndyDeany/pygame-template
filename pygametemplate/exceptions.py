27cf8b8843e7f93f11aabbc5184feb67ade87cf2
AndyDeany <oneandydean@hotmail.com>
2017-10-07 21:58:37 +0100

    Add pygametemplate.exceptions.PygameError as an alias for pygame.error

--- pygametemplate/exceptions.py
+++ pygametemplate/exceptions.py
@@ -1,3 +1,8 @@
+"""Module containing all the exceptions included in the pygametemplate module."""
+from pygame import error as PygameError
+
+
 class CaughtFatalException(Exception):
     """Raised by pygametemplate.core.log() when it catches a fatal exception."""
+
     pass
