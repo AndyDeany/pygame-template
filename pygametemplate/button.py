23ab8664d1ed16ea0339f9b94938e1c95b574132
AndyDeany <oneandydean@hotmail.com>
2017-10-30 15:40:57 +0000

    Remove silly try/except blocks in button.py

--- pygametemplate/button.py
+++ pygametemplate/button.py
@@ -7,16 +7,14 @@ class Button(object):
     """Class representing keyboard keys."""
     def __init__(self, game, number):
         self.game = game
-        try:
-            self.number = number
-            self.event = None   # The last event that caused the button press
 
-            self.pressed = 0    # If the button was just pressed
-            self.held = 0       # If the button is held
-            self.released = 0   # If the button was just released
-            self.press_time = 0.0
-        except Exception:
-            log("Failed to initialise button variable")
+        self.number = number
+        self.event = None   # The last event that caused the button press
+
+        self.pressed = 0    # If the button was just pressed
+        self.held = 0       # If the button is held
+        self.released = 0   # If the button was just released
+        self.press_time = 0.0
 
     def press(self):
         self.pressed = 1
@@ -28,17 +26,11 @@ class Button(object):
         self.released = 1
 
     def reset(self):
-        try:
-            self.pressed = 0
-            self.released = 0
-        except Exception:
-            log("Failed to reset button")
+        self.pressed = 0
+        self.released = 0
 
     def time_held(self):
-        try:
-            if self.held:
-                return time.time() - self.press_time
-            else:
-                return 0.0
-        except Exception:
-            log("Failed to get button held time")
+        if self.held:
+            return time.time() - self.press_time
+        else:
+            return 0.0
