ccfc30476337c72e6725b347d043c745a60b9ae3
AndyDeany <oneandydean@hotmail.com>
2017-10-30 10:42:24 +0000

    Move metadata from pygametemplate/__init__.py to setup.py to avoid import errors

--- pygametemplate/__init__.py
+++ pygametemplate/__init__.py
@@ -2,15 +2,6 @@
 from __future__ import absolute_import
 
 
-__version__ = "0.6.0"
-
-__url__ = "https://github.com/AndyDeany/pygame-template"
-__download_url__ = "{}/archive/v{}.tar.gz".format(__url__, __version__)
-
-__author__ = "Andrew Dean"
-__author_email__ = "oneandydean@hotmail.com"
-
-
 from pygametemplate.core import log, load_image, load_font
 from pygametemplate.game import Game
 from pygametemplate.view import View
