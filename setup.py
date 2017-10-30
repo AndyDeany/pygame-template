29236db6d20d9039b5c32f34b1046466c86e3cf5
AndyDeany <oneandydean@hotmail.com>
2017-10-30 11:53:44 +0000

    Update classifiers to represent currently supported python versions

--- setup.py
+++ setup.py
@@ -25,9 +25,9 @@ setup(
         "Natural Language :: English",
         "Topic :: Software Development :: Libraries :: pygame",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
     )
 )
